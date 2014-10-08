99f56907e1df186be1e0879617cc085df74aaaf1
Raphael <raphael@hometime.fr>
2014-10-08 18:25:58 +0200

    Last 404 from watch details page, new link in notify mail

--- web/launch_activator.bat
+++ web/launch_activator.bat
@@ -1,7 +1,7 @@
 
 d:
 set JAVA_HOME=D:\Autres\Developpement\java\jdk1.8.0_20
-set JAVA_OPTS=-Djavax.net.ssl.trustStore="D:\Autres\Developpement\Eclipse\luna-git-workspace\hometime\web\conf\keystore" -Djavax.net.ssl.trustStorePassword="rafoufou" %JAVA_OPTS%
+#set JAVA_OPTS=-Djavax.net.ssl.trustStore=D:\Autres\Developpement\Eclipse\luna-git-workspace\hometime\web\conf\keystore -Djavax.net.ssl.trustStorePassword=rafoufou %JAVA_OPTS%
 set PATH=%JAVA_HOME%\bin;%PATH%
 set PATH=D:\Autres\Developpement\typesafe-activator-1.2.10-minimal;%PATH%
 cd D:\Autres\Developpement\Eclipse\luna-git-workspace\hometime\web
