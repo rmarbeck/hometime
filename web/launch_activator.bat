cbdd48fe542cb6e9cf5479e50c93066bfd0fef81
Raphaël Marbeck <raphael@hometime.fr>
2018-10-02 11:04:31 +0200

    Address in selling document to new company address

--- web/launch_activator.bat
+++ web/launch_activator.bat
@@ -2,7 +2,7 @@
 c:
 set JAVA_HOME=C:\Developpement\Java\jdk1.8.0_25
 #set JAVA_OPTS=-Djavax.net.ssl.trustStore=C:\Developpement\git\hometime\web\conf\keystore -Djavax.net.ssl.trustStorePassword=rafoufou %JAVA_OPTS%
-set JAVA_OPTS=-Djavax.net.debug=ssl -Djavax.net.ssl.trustStore=C:\Developpement\git\hometime\web\conf\keystore -Djavax.net.ssl.trustStorePassword=rafoufou %JAVA_OPTS%
+#set JAVA_OPTS=-Djavax.net.debug=ssl -Djavax.net.ssl.trustStore=C:\Developpement\git\hometime\web\conf\keystore -Djavax.net.ssl.trustStorePassword=rafoufou %JAVA_OPTS%
 set PATH=%JAVA_HOME%\bin;%PATH%
 set PATH=C:\Developpement\typesafe-activator-1.2.10-minimal;%PATH%
 set SYSTEM_PAY_TEST_KEY=5240800751587805
