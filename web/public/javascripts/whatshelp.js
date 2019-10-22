bf5d9f147a8b5a4bd7401cf887770c28213bff1a
rmarbeck <raphael@hometime.fr>
2019-10-22 14:26:42 +0200

    Removing whatshelp for safari browsers

--- web/public/javascripts/whatshelp.js
+++ web/public/javascripts/whatshelp.js
@@ -1,5 +1,7 @@
 
-    (function () {
+	var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+    function wh() {
         var options = {
         	email: "wcontact@@hometime.fr", // Email
             sms: "+33782434751", // Sms phone number
@@ -14,4 +16,9 @@
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
         s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
         var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    })();
+    };
+    
+    if (!isSafari) { 
+    	wh();
+    }
+    
