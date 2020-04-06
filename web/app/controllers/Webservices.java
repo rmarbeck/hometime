bb60d1dfc8478ef29c93bf6a03c6f5fd69bdf883
Raphaël Marbeck <raphael@hometime.fr>
2020-04-06 18:25:39 +0200

    Missing static modifiers

--- web/app/controllers/Webservices.java
+++ web/app/controllers/Webservices.java
@@ -9,11 +9,11 @@ import models.Brand;
 import models.Feedback;
 
 public class Webservices extends Controller {
-	public Result getBrands() {
+	public static Result getBrands() {
 		return ok(Json.toJson(Brand.findAll()));
 	}
 	
-	public Result getFeedbacks() {
+	public static Result getFeedbacks() {
 		return ok(Json.toJson(Feedback.findAll()));
 	}
 }
