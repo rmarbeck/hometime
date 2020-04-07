cc59a571f16b2146bb95ece573d2cf749dff2dd0
Raphaël Marbeck <raphael@hometime.fr>
2020-04-07 12:22:41 +0200

    Testing secret key via header

--- web/app/controllers/Webservices.java
+++ web/app/controllers/Webservices.java
@@ -2,7 +2,7 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-
+import play.Logger;
 import play.libs.Json;
 
 import models.Brand;
@@ -10,10 +10,12 @@ import models.Feedback;
 
 public class Webservices extends Controller {
 	public static Result getBrands() {
+		Logger.debug("Validating secretKey, value received is : [{}]", request().getHeader("secretKey"));
 		return ok(Json.toJson(Brand.findAll()));
 	}
 	
 	public static Result getFeedbacks() {
+		Logger.debug("Validating secretKey, value received is : [{}]", request().getHeader("secretKey"));
 		return ok(Json.toJson(Feedback.findAll()));
 	}
 }
