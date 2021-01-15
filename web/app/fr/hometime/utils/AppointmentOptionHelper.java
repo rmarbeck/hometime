8b5251821cb42b8e5e87311f7b336fe3ba9930e7
rmarbeck <raphael@hometime.fr>
2021-01-15 08:11:48 +0100

    After ff

--- web/app/fr/hometime/utils/AppointmentOptionHelper.java
+++ web/app/fr/hometime/utils/AppointmentOptionHelper.java
@@ -28,6 +28,8 @@ public class AppointmentOptionHelper {
 	private static int APPOINTMENT_DEFAULT_DURATION_IN_MINUTES = 30;
 	private static int APPOINTMENT_DEFAULT_DELAY_BEFORE_NEXT_AVAILABLE_IN_MINUTES = 90;
 	private static LocalTime APPOINTMENT_DEFAULT_FIRST_HOUR_AVAILABLE = LocalTime.MIDNIGHT.plusHours(10);
+	
+	//Special hour of closinf (usualy 19)
 	private static LocalTime APPOINTMENT_DEFAULT_LAST_HOUR_AVAILABLE = LocalTime.MIDNIGHT.plusHours(18);
 	private static List<DayOfWeek> DAYS_OPEN = Arrays.asList(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY);
 	
