1e7c513129e1e2345f13cd3cab93741cd8d0e3ce
Raphaël Marbeck <raphael@hometime.fr>
2021-03-14 12:09:30 +0100

    Trying something else for DB issue, and removing explicit use of java 8 to avoid compiling warning

--- web/app/models/PaymentsReport.java
+++ web/app/models/PaymentsReport.java
@@ -3,7 +3,6 @@ package models;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.function.Supplier;
 
 public class PaymentsReport {
 	private final static String METHOD_KEY = "admin.report.payment.method.";
@@ -27,20 +26,21 @@ public class PaymentsReport {
 	}
 	
 	public static List<PaymentsReport> generateReportEnhanced() {
-		return generateReportEnhanced(() -> Payment.findAllByInBankDateDescForReporting());
+		return generateReportEnhanced(Payment.findAllByInBankDateDescForReporting());
 	}
 	
 	public static List<PaymentsReport> generateReportEnhancedCreationDateDESC() {
-		return generateReportEnhanced(() -> Payment.findAllByCreationDateDescForReporting());
+		return generateReportEnhanced(Payment.findAllByCreationDateDescForReporting());
 	}
-	
-	private static List<PaymentsReport> generateReportEnhanced(Supplier<List<Payment>> paymentReader) {
+
+	private static List<PaymentsReport> generateReportEnhanced(List<Payment> paymentList) {
 		List<PaymentsReport> report = new ArrayList<PaymentsReport>();
-		List<Payment> payments = paymentReader.get();
+		List<Payment> payments = paymentList;
 		if (listNotEmpty(payments))
 			payments.stream().forEach(payment -> report.add(new PaymentsReport(payment)));
 		return report;
 	}
+	
 	
 	private static boolean listNotEmpty(List<?> list) {
 		return (list != null && list.size() != 0);
