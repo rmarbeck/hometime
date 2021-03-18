8859ae2e68471f8af9b7b9fd635ca52a55100cf9
Raphaël Marbeck <raphael@hometime.fr>
2021-03-18 20:15:47 +0100

    Trying to keep alive connection from server

--- web/app/actors/DashboardActor.java
+++ web/app/actors/DashboardActor.java
@@ -1,20 +1,17 @@
 package actors;
 
-import java.time.Instant;
-import java.util.stream.Collectors;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
-import models.AppointmentRequest;
-import models.OrderRequest;
-import play.libs.Json;
-
 import play.Logger;
+import play.libs.Akka;
+import scala.concurrent.duration.FiniteDuration;
 
 public class DashboardActor extends UntypedActor {
+	private static final long KEEP_ALIVE_FREQUENCY = 40l;
+	private static final String KEEP_ALIVE_UNIT = "seconds";
 	
     public static Props props(ActorRef out, ActorRef manager) {
         return Props.create(DashboardActor.class, out, manager);
@@ -24,11 +21,18 @@ public class DashboardActor extends UntypedActor {
     private final ActorRef manager;
 
     public DashboardActor(ActorRef out, ActorRef manager) {
-    	Logger.debug("Starting actor {"+this.self().path()+"}");
+    	Logger.info("Starting actor {"+this.self().path()+"}");
         this.out = out;
         this.manager = manager;
        
         this.self().tell(new InitMessage(), null);
+        Akka.system().scheduler().schedule(
+        		FiniteDuration.apply(KEEP_ALIVE_FREQUENCY, KEEP_ALIVE_UNIT),
+        		FiniteDuration.apply(KEEP_ALIVE_FREQUENCY, KEEP_ALIVE_UNIT),
+                this.getSelf(),
+                "ping",
+                Akka.system().dispatcher(),
+                null);
     }
     
     @Override
@@ -45,13 +49,13 @@ public class DashboardActor extends UntypedActor {
 			Logger.debug("Forwarding message {"+this.self().path()+"}");
 			out.tell(((ForwardJsonMessage) message).getJsonMessage(), self());
 		} else {
-			ObjectMapper mapper = new ObjectMapper();
+			Logger.info("Actor just recevied an unsupported message ["+message.getClass()+"]");
 		}
 	}
 
 	@Override
 	public void postStop() throws Exception {
-		Logger.debug("Actor Stopped {"+this.self().path()+"}");
+		Logger.info("Actor Stopped {"+this.self().path()+"}");
 		super.postStop();
 	}
 
