2b201591f7cf5b3068387e079bcc60a233cbf046
Raphaël Marbeck <raphael@hometime.fr>
2021-03-22 17:35:53 +0100

    New dash replaces old one

--- web/app/controllers/Dashboard.java
+++ web/app/controllers/Dashboard.java
@@ -5,38 +5,32 @@ import com.fasterxml.jackson.databind.JsonNode;
 import actors.DashboardActor;
 import actors.DashboardManager;
 import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
 import akka.actor.Props;
-import models.BuyRequest;
-import models.CustomerWatch;
-import models.OrderRequest;
 import play.libs.Akka;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.WebSocket;
 import play.mvc.With;
-import scala.concurrent.duration.FiniteDuration;
-import views.html.*;
 import views.html.admin.dashboard_ws;
 
 @SecurityEnhanced.Authenticated(value=SecuredEnhanced.class, rolesAuthorized =  {models.User.Role.ADMIN, models.User.Role.MASTER_WATCHMAKER, models.User.Role.COLLABORATOR, models.User.Role.PARTNER, models.User.Role.CUSTOMER})
 @With(NoCacheAction.class)
 public class Dashboard extends Controller {
-	public static final ActorRef manager = Akka.system().actorOf(Props.create(DashboardManager.class), "DashBoardManager2");
+	public static final ActorRef manager = Akka.system().actorOf(Props.create(DashboardManager.class), "DashBoardManager");
 	@SecurityEnhanced.Authenticated(value=SecuredEnhanced.class, rolesAuthorized =  {models.User.Role.ADMIN, models.User.Role.COLLABORATOR})
 	public static WebSocket<JsonNode> socket() {
 		System.err.println("Starting websocket");
-		System.err.println("----> "+manager.path());
 	    return WebSocket.withActor((out) -> DashboardActor.props(out, manager));
 	}
 	
 	@SecurityEnhanced.Authenticated(value=SecuredEnhanced.class, rolesAuthorized =  {models.User.Role.ADMIN, models.User.Role.COLLABORATOR})
-	public static Result display() {
-		return ok(dashboard_ws.render("", 1));
+	public static Result defaultDash() {
+		return display(1);
     }
 	
-    public static Result test_websocket() {
-    	return ok(test_websocket.render("toto"));
+	@SecurityEnhanced.Authenticated(value=SecuredEnhanced.class, rolesAuthorized =  {models.User.Role.ADMIN, models.User.Role.COLLABORATOR})
+	public static Result display(Integer page) {
+		return ok(dashboard_ws.render("", page));
     }
 }
 
