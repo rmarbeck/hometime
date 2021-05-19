7f90e7e011735b98c79ff91cc96c758bbb1bd599
rmarbeck <raphael@hometime.fr>
2021-05-19 09:49:55 +0200

    Appointments update after new covid rules

--- web/app/fr/hometime/utils/AppointmentOptionHelper.java
+++ web/app/fr/hometime/utils/AppointmentOptionHelper.java
@@ -26,7 +26,7 @@ import models.AppointmentRequest;
 
 public class AppointmentOptionHelper {
 	private static int APPOINTMENT_DEFAULT_DURATION_IN_MINUTES = 30;
-	private static int APPOINTMENT_DEFAULT_DELAY_BEFORE_NEXT_AVAILABLE_IN_MINUTES = 90;
+	private static int APPOINTMENT_DEFAULT_DELAY_BEFORE_NEXT_AVAILABLE_IN_MINUTES = 60;
 	private static LocalTime APPOINTMENT_DEFAULT_FIRST_HOUR_AVAILABLE = LocalTime.MIDNIGHT.plusHours(10);
 	
 	//Special hour of closing (usualy 19)
